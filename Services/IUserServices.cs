9abdfe838b19c1a4ac25095c8d1cfd37d2e8cdd1
Anto <antonella.cortes@udla.edu.ec>
2023-01-31 11:21:31 -0500

    Add Interfaces to Services to help implement CRUD functionalities.

---
+++ Services/IUserServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoFinalProgramacion.Models;
+
+namespace ProyectoFinalProgramacion.Services
+{
+    public interface UserServices
+    {
+        Task<List<User>> GetUsersList();
+        Task<int> AddUser(User userModel);
+        Task<int> DeleteUser(User userModel);
+        Task<int> UpdateUser(User userModel);
+    }
+}
