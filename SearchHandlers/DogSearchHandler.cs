248bbcb02f000dad1e0b554c40891fc5384abbdb
Anto <antonella.cortes@udla.edu.ec>
2023-01-31 11:19:55 -0500

    Create SearchHandlers to navigate the DB from Dog and User.

---
+++ SearchHandlers/DogSearchHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoFinalProgramacion.Models;
+
+namespace ProyectoFinalProgramacion.SearchHandlers
+{
+    public class DogSearchHandler : SearchHandler
+    {
+        public IList<Dog> Dogs { get; set; }
+        public string NavigationRoute { get; set; }
+        public Type NavigationType { get; set; }
+        protected override void OnQueryChanged(string oldValue, string newValue)
+        {
+            base.OnQueryChanged(oldValue, newValue);
+
+            if (string.IsNullOrWhiteSpace(newValue))
+            {
+                ItemsSource = null;
+            }
+            else
+            {
+                ItemsSource = Dogs.Where(dog => dog.Name.ToLower().Contains(newValue.ToLower())).ToList();
+            }
+        }
+
+        protected override async void OnItemSelected(object item)
+        {
+            base.OnItemSelected(item);
+            var navParam = new Dictionary<string, object>();
+            navParam.Add("DogDetail", item);
+            if (!string.IsNullOrWhiteSpace(NavigationRoute))
+            {
+                await Shell.Current.GoToAsync(NavigationRoute, navParam);
+            }
+        }
+    }
+}
